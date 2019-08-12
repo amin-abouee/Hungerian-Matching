9c3e5c975dd3da23408698a9d00191c98847585f
Amin Abouee <Amin.Abouee@gmail.com>
2019-08-12 17:36:31 +0200

    add bfs function

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <queue>
 #include <functional>
 #include <algorithm>
+#include <queue>
 
 #include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
@@ -14,28 +15,78 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
+struct vertex
+{
+    // cv::Point2f position;
+    std::vector <int> adjacencyLists;
+    std::vector <double> weights;
+    double label;
+    bool free;
+    int matchedIdx;
+
+    vertex()
+    {
+        adjacencyLists.reserve(100);
+        weights.reserve(100);
+        label = 0.0;
+        free = true;
+        matchedIdx = -1;
+    };
+};
+
+
+std::set <std::pair <int, int>  >findBFSPatches ( std::vector<vertex>& A, std::vector<vertex>&B, int u, int y, std::map<int, int>& mapImageIndexToVectorIdx)
+{
+    std::vector <int> parentsA (A.size(), -1);
+    std::vector <int> parentsB (B.size(), -1);
+
+    std::queue<int> Q;
+    Q.push(u);
+    parentsA[u] = u;
+
+    while(!Q.empty())
+    {
+        int node = Q.front();
+        Q.pop();
+        for(int i(0); i < A[node].adjacencyLists.size(); i++)
+        {
+            int b = A[node].adjacencyLists[i];
+            if (b == y)
+            {
+                while(!Q.empty())
+                    Q.pop();
+                break;
+            }
+            int vecBidx = mapImageIndexToVectorIdx[b];
+            if (A[node].label + B[vecBidx].label == A[node].weights[i] && parentsB[vecBidx] == -1)
+            {
+                parentsB[vecBidx] = node;
+                for(int j(0); j < B[vecBidx].adjacencyLists.size(); j++)
+                {
+                    int a = B[vecBidx].adjacencyLists[j];
+                    if (B[vecBidx].label + A[a].label == B[vecBidx].weights[j] && parentsA[a] == -1)
+                    {
+                        Q.push(a);
+                        parentsA[a] = b;
+                    }
+                }
+            }
+        }
+    }
+
+    int idx = y;
+    std::set< std::pair < int, int> > M;
+    while(parentsA[idx] != idx)
+    {
+        int vecBidx = mapImageIndexToVectorIdx[idx];
+        M.insert(std::make_pair(idx, vecBidx));
+        idx = parentsB[vecBidx];
+    }
+    return M;
+}
 
 int main(void)
 {
-
-    struct vertex
-    {
-        // cv::Point2f position;
-        std::vector <int> adjacencyLists;
-        std::vector <double> weights;
-        double label;
-        bool free;
-        int matchedIdx;
-
-        vertex()
-        {
-            adjacencyLists.reserve(100);
-            weights.reserve(100);
-            label = 0.0;
-            free = true;
-            matchedIdx = -1;
-        };
-    };
 
     // std::vector < std::vector <int> > adjacencyLists;
     // std::vector <double> weights;
@@ -46,7 +97,7 @@ int main(void)
     //-----------
     
     //cv::Mat imageBGR(cv::Size(1200, 800), CV_8UC4, cv::Scalar(64, 64, 64));
-    cv::Mat imageBGR = cv::imread("/Users/amin/Workspace/cplusplus/Hungerian-Matching/matching.png", cv::IMREAD_UNCHANGED);
+    cv::Mat imageBGR = cv::imread("/home/amin/Workspace/cplusplus/graph_matching/matching.png", cv::IMREAD_UNCHANGED);
 
     cv::Mat imagePointsMat;
     cv::Mat modelPointsMat;
@@ -63,7 +114,7 @@ int main(void)
     std::vector<float> imageAngles; // Feature orientation in Radians
     std::vector<float> modelAngles;
     
-    cv::FileStorage fs("/Users/amin/Workspace/cplusplus/Hungerian-Matching/matching.yml", cv::FileStorage::READ);
+    cv::FileStorage fs("/home/amin/Workspace/cplusplus/graph_matching/matching.yml", cv::FileStorage::READ);
 
 
     fs["imagePoints"] >> imagePointsMat;
@@ -186,6 +237,88 @@ int main(void)
         }
     }
 
+
+    {
+        A.clear();
+        B.clear();
+        mapImageIndexToVectorIdx.clear();
+
+        vertex a1;
+        a1.adjacencyLists.push_back(0);
+        a1.weights.push_back(2);
+        a1.adjacencyLists.push_back(1);
+        a1.weights.push_back(3);
+        a1.adjacencyLists.push_back(2);
+        a1.weights.push_back(2);
+        a1.label = 3;
+        A.push_back(a1);
+
+        vertex a2;
+        a2.adjacencyLists.push_back(0);
+        a2.weights.push_back(3);
+        a2.adjacencyLists.push_back(1);
+        a2.weights.push_back(4);
+        a2.adjacencyLists.push_back(2);
+        a2.weights.push_back(5);
+        a2.label = 5;
+        A.push_back(a2);
+
+        vertex a3;
+        a3.adjacencyLists.push_back(1);
+        a3.weights.push_back(1);
+        a3.adjacencyLists.push_back(2);
+        a3.weights.push_back(3);
+        a3.adjacencyLists.push_back(3);
+        a3.weights.push_back(4);
+        a3.label = 4;
+        A.push_back(a3);
+
+        vertex a4;
+        a4.adjacencyLists.push_back(2);
+        a4.weights.push_back(2);
+        a4.adjacencyLists.push_back(3);
+        a4.weights.push_back(4);
+        a4.label = 4;
+        A.push_back(a4);
+
+        vertex b1;
+        b1.adjacencyLists.push_back(0);
+        b1.weights.push_back(2);
+        b1.adjacencyLists.push_back(1);
+        b1.weights.push_back(3);
+        B.push_back(b1);
+
+        vertex b2;
+        b2.adjacencyLists.push_back(0);
+        b2.weights.push_back(3);
+        b2.adjacencyLists.push_back(1);
+        b2.weights.push_back(4);
+        b2.adjacencyLists.push_back(2);
+        b2.weights.push_back(1);
+        B.push_back(b2);
+
+        vertex b3;
+        b3.adjacencyLists.push_back(0);
+        b3.weights.push_back(2);
+        b3.adjacencyLists.push_back(1);
+        b3.weights.push_back(3);
+        b3.adjacencyLists.push_back(3);
+        b3.weights.push_back(2);
+        B.push_back(b3);
+
+        vertex b4;
+        b4.adjacencyLists.push_back(2);
+        b4.weights.push_back(4);
+        b4.adjacencyLists.push_back(3);
+        b4.weights.push_back(4);
+        B.push_back(b4);
+
+        mapImageIndexToVectorIdx[0] = 0;
+        mapImageIndexToVectorIdx[1] = 1;
+        mapImageIndexToVectorIdx[2] = 2;
+        mapImageIndexToVectorIdx[3] = 3;
+    }
+
     // double check the connectivity between model idx and image idx, image idx maps to vector idx
     for(const auto& p : mapImageIndexToVectorIdx)
         std::cout << "connect: " << p.first << " , " << p.second << std::endl;
@@ -206,9 +339,11 @@ int main(void)
     std::set<int> S;
     std::set<int> T;
     std::set<int> NS;
+    std::set <std::pair<int , int> > M;
 
     while(true)
     {
+        int u = -1;
         // initialization of S and T, if necessary
         if (initSets == true)
         {
@@ -217,6 +352,7 @@ int main(void)
                 if(A[i].free == true)
                 {
                     S.insert(i);
+                    u = i;
                     break;
                 }
             }
@@ -266,9 +402,9 @@ int main(void)
             for(const auto& ns : NS)
             {
                 const int vecBIdx = mapImageIndexToVectorIdx[ns];
-                if (B[vecBIdx].label == true &&  T.find(vecBIdx) != T.end())
+                if (B[vecBIdx].free == true && T.find(vecBIdx) == T.end())
                 {
-
+                    findBFSPatches (A, B, u, ns, mapImageIndexToVectorIdx);
                 }
                 else
                 {
